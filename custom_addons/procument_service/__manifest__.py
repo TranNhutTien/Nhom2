e3441a8deac1d2a3a55f4525580fb9fd513aedac
NhutTien <zrov270423@gmail.com>
2024-05-29 14:53:13 +0700

    Sửa lỗi

--- custom_addons/procument_service/__manifest__.py
+++ custom_addons/procument_service/__manifest__.py
@@ -3,9 +3,11 @@
     "name": "Procument Service",
     "license": "AGPL-3",
     "author": "Nhom 2",
-    "depends": [],
+    "depends": ["product"],
     "application": True,
     "category": "Services/Procument Service",
     "data":[
+        "views/procument_service_details.xml",
+        "views/menu.xml",
     ],
 }
