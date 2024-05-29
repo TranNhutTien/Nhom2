7d5e558b8c6ce658fef14c7564ff95d584adcaa4
NhutTien <zrov270423@gmail.com>
2024-05-29 14:36:19 +0700

    Khởi tạo module procument_service

---
+++ custom_addons/procument_service/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Procument Service",
+    "license": "AGPL-3",
+    "author": "Nhom 2",
+    "depends": [],
+    "application": False,
+    "data":[
+    ],
+}
