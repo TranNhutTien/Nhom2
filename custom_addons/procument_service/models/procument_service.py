b6fd72dba2d520eaa40d556f507fb2d116a93c04
NhutTien <zrov270423@gmail.com>
2024-05-29 15:05:22 +0700

    Thêm trạng thái ban đầu là New

--- custom_addons/procument_service/models/procument_service.py
+++ custom_addons/procument_service/models/procument_service.py
@@ -4,12 +4,18 @@ class ProcumentService(models.Model):
     _name = "procument.service"
     _description = "Procument Service"
    
-    name = fields.Char(string='Order Reference')
+    name = fields.Char(string='Order Reference', readonly=True, default=lambda self: _('New'))
     date = fields.Date(string="Order Date", default=fields.Date.context_today)
     ref = fields.Char(string="Reference")
     customer_id = fields.Many2one("res.partner", string="Customer", required=True)
     vendor_id = fields.Many2one("res.partner", string="Vendor", required=True)
-    
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', _('New')) == _('New'):
+            vals['name'] = self.env['ir.sequence'].next_by_code('procument.service') or _('New')
+        res = super(ProcumentService, self).create(vals)
+        return res
 
     
 
