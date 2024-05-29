9f132e102e4882495be2cf25dfcbc91e8ffc95f1
NhutTien <zrov270423@gmail.com>
2024-05-29 12:39:13 +0700

    Khởi tạo module product_extension

---
+++ custom_addons/product_extension/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Product Extension",
+    "license": "AGPL-3",
+    "author": "Nhom 2",
+    "depends": ["product"],
+    "application": False,
+    "data":[
+       
+    ],
+}
