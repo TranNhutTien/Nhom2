6a3cc9740bd802db87f6e1a9af9984b93594bdae
NhutTien <zrov270423@gmail.com>
2024-05-29 14:05:43 +0700

    Tạo file views, thêm view invoice_extension: kế thừa view tạo invoice và thêm trường mới

--- custom_addons/invoice_extension/models/invoice_extension.py
+++ custom_addons/invoice_extension/models/invoice_extension.py
@@ -2,8 +2,3 @@ from odoo import fields, models, api
 class InvoiceExtension(models.Model):
     _inherit = "account.move"
     previous_invoice = fields.Many2one("account.move", domain="[('move_type', '=', 'out_invoice')]", string="Previous Invoice")
-    
-    
-
-
-
