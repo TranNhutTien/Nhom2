6104e4ba21971861c9654ca0cc108b0ddebf7e10
NhutTien <zrov270423@gmail.com>
2024-05-29 14:01:41 +0700

    Khởi tạo module invoice_extension

---
+++ custom_addons/invoice_extension/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Invoice Extension",
+    "license": "AGPL-3",
+    "author": "Nhom 2",
+    "depends": ["account"],
+    "application": False,
+    "data":[
+    ],
+}
